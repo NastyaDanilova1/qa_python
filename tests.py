148a2aaf23ab08eaf22dee5eaa8e6cadf37c88e5
Artyr Zorin <zorina@onlinepatent.ru>
2024-04-19 16:52:47 +0700

    fix reviews

--- tests.py
+++ tests.py
@@ -16,7 +16,6 @@ class TestBooksCollector:
         collector.add_new_book("Властелин колец - Две башни")
         assert len(collector.get_books_genre()) == 2
 
-
     @pytest.mark.parametrize(
         "name", ["", "Легенды о короле Артуре и рыцарях Круглого Стола"]
     )
@@ -24,12 +23,10 @@ class TestBooksCollector:
         books_collector.add_new_book(name)
         assert name not in books_collector.get_books_genre()
 
-
     def test_get_book_genre_return_genre_by_name(self, books_collector):
         name = "Синий трактор"
         books_collector.add_new_book(name)
         assert books_collector.get_book_genre(name) == ""
-
 
     def test_set_book_genre_add_genre(self, books_collector):
         name = "Кошмар на улице вязов"
@@ -38,7 +35,6 @@ class TestBooksCollector:
         books_collector.set_book_genre(name, genre)
         assert books_collector.get_book_genre(name) == genre
 
-
     def test_get_books_with_specific_genre_by_genre(self, books_collector):
         name = "Дневник Бриджит Джонс"
         genre = "Комедии"
@@ -46,17 +42,15 @@ class TestBooksCollector:
         books_collector.set_book_genre(name, genre)
         assert books_collector.get_books_with_specific_genre(genre) == [name]
 
-
     def test_get_books_genre_dict_books_genre_returned(
-        self, books_collector, add_books, set_genre
+            self, books_collector, add_books, set_genre
     ):
         returned_books_genre_dict = books_collector.get_books_genre()
         expected_books_genre_dict = names_and_genres_of_books
         assert returned_books_genre_dict == expected_books_genre_dict
 
-
     def test_get_books_for_children_return_non_rating_books(
-        self, books_collector, add_books, set_genre
+            self, books_collector, add_books, set_genre
     ):
         expected_books = [
             "Властелин колец - Две башни",
@@ -66,16 +60,14 @@ class TestBooksCollector:
         returned_books = books_collector.get_books_for_children()
         assert returned_books == expected_books
 
-
     def test_add_book_in_favorites_not_added_book_twice_in_favorite_book(
-        self, books_collector
+            self, books_collector
     ):
         books_collector.add_new_book("Дневник Бриджит Джонс")
         books_collector.add_book_in_favorites("Дневник Бриджит Джонс")
         books_collector.add_book_in_favorites("Дневник Бриджит Джонс")
         favorite_books = books_collector.get_list_of_favorites_books()
         assert len(favorite_books) == 1
-
 
     def test_add_book_in_favorite_one_added_book(self, books_collector):
         book = "Властелин колец - Две башни"
@@ -84,16 +76,14 @@ class TestBooksCollector:
         favorite_books = books_collector.get_list_of_favorites_books()
         assert len(favorite_books) == 1 and favorite_books[0] == book
 
-
     def test_delete_book_from_favorites_not_deleted_book_if_book_not_in_favorite_books(
-        self, books_collector
+            self, books_collector
     ):
         books_collector.add_new_book("Дневник Бриджит Джонс")
         books_collector.add_book_in_favorites("Дневник Бриджит Джонс")
         books_collector.delete_book_from_favorites("Синий трактор")
         favorite_books = books_collector.get_list_of_favorites_books()
         assert len(favorite_books) == 1
-
 
     def test_delete_book_from_favorite_book_deleted(self, books_collector):
         books_collector.add_new_book("Дневник Бриджит Джонс")
@@ -102,9 +92,8 @@ class TestBooksCollector:
         favorite_books = books_collector.get_list_of_favorites_books()
         assert len(favorite_books) == 0
 
-
     def test_get_list_of_favorites_books_returned_list(
-        self, books_collector, add_books
+            self, books_collector, add_books
     ):
         for book in books_collector.get_books_genre():
             books_collector.add_book_in_favorites(book)
